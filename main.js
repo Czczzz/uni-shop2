519286f5a09da35c0a92adb02f9aaaaadb8dc25e
陈子聪 <254612409@qq.com>
2021-01-07 10:09:28 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 import Vue from 'vue'
 import App from './App'
 
+// 按需导入 $http 对象(导入网络请求的包)
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做的一些事情（请求拦截器）
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情（响应拦截器）
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装请求数据的消息提示弹框
+uni.$showMsg = function(title = "数据请求失败", duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,146 @@
 <template>
-	<view>
-		
-	</view>
+  <view>
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+      <swiper-item v-for="(item,i) in swiperList" :key="i">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+
+    <!-- 分类导航区域 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navSwitch(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+    <!-- 首页楼层区域 -->
+    <view class="floor-list">
+      <view class="floor-item" v-for="(item,i) in floorList" :key="i">
+        <image :src="item.floor_title.image_src" class="floor-img" ></image>
+        <!-- 楼层图片区域 -->
+         <view class="floor-img-box">
+           <!-- 左侧盒子 -->
+           <navigator class="left-img-box" :url="item.product_list[0].url">
+             <image :src="item.product_list[0].image_src" mode="widthFix":style="{width: item.product_list[0].image_width + 'rpx'}" ></image>
+           </navigator>
+           <!-- 右侧盒子 -->
+           <view class="right-img-box">
+             <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+               <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+             </navigator>
+           </view>
+         </view>
+      </view>
+    
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+  export default {
+    data() {
+      return {
+        // 轮播图的数据列表
+        swiperList: [],
+        // 分类选项数据列表
+        navList: [],
+        // 首页楼层数据列表
+        floorList: []
+      };
+    },
+    onLoad() {
+      // 一加载就获取轮播图的数据
+      this.getSwiperList(),
+        // 一加载就获取分类列表的数据
+        this.getnavList()
+      this.getfloorList()
+    },
+    methods: {
+      // 获取轮播图
+      async getSwiperList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // console.log(res)
+        // 请求失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功
+        this.swiperList = res.message
+      },
+      // 获取分类列表
+      async getnavList() {
+        const {
+          data: res
+        } = await uni.$http.get("/api/public/v1/home/catitems")
+        // console.log(res)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.navList = res.message
+        // console.log(this.navList)
+      },
+      // 点击分类图标切换到分类tabber
+      navSwitch(item) {
+        if (item.name === "分类") {
+          uni.switchTab({
+            url: "/pages/cate/cate"
+          })
+        }
+      },
+      // 获取首页楼层数据
+      async getfloorList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/floordata')
+        console.log(res)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 通过双层forEach循环,处理url地址
+        res.message.forEach(floor=>{
+          floor.product_list.forEach(prod=>{
+           prod.url='/subpkg/goods_list/goods_list?'+prod.navigator_url.split('?')[1]
+          })
+          })
+        this.floorList = res.message
+      }
+    },
+  }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 330rpx;
 
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+  .floor-img {
+    width: 100%;
+    height: 60rpx;
+    display: flex;
+  }
+  
+  .right-img-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
+  }
 </style>
